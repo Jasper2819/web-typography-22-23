4101c83756ca7344977fd5cc66bccd32357d9716
Jasper2819 <jasperriepe@gmail.com>
2022-10-24 18:20:41 +0200

    Java sound changes
    It took me a lot of time to figure out how the timing worked. A lot of trial and error, but i figured it out and made it for myself clear what time belongs to which animation.

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,15 +1,16 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0,
-	9,
-	12,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+	0,		
+	9,		//sound 1 op 9 sec
+	12,		//sound 2
+	15,		//3
+	16.5,	// 4
+	19,		//5
+	25.5,	//6	
+	28,		//7
+	31.5,	//8
+	33,		//9
+	34,		//10
+	86		//sound 11
 ];
